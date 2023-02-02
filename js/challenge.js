861c3a58c33b0f73f97dbaba0cde300afbbc7977
Regan Svoboda <regansvoboda@gmail.com>
2023-02-02 11:07:08 -0600

    count

--- js/challenge.js
+++ js/challenge.js
@@ -1,9 +1,23 @@
-let button = document.querySelector('#plus')
-button.addEventListener('click', incrementCounter)
+let count = document.querySelector('#counter')
+console.log(count)
 
+let plusButton = document.querySelector('#plus')
+plusButton.addEventListener('click', incrementCounter)
 
 function incrementCounter(eventObject) {
-  console.log(eventObject) 
+    count++
 }
+
+
+
+
+
+
+
+
+// let minusButton = document.querySelector('#minus')
+// minusButton.addEventListener('click', decrementCounter)
+
+// function decrementCounter()
 
 
